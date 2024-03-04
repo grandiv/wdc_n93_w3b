d909f9831b0d7a46fc832ffb5b1c24fe292d1d88
johanpramudito <ajohanpramudito@gmail.com>
2024-03-04 20:51:02 +0700

    push yogyakarta jadi

---
+++ components/pages/gunungkidul/Description/Description.js
@@ -0,0 +1,80 @@
+import { React, useEffect } from "react";
+import Image from "next/image";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { aosDuration, getDelay } from "../../../allPage/configs/aosConfig";
+
+export default function Description() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  return (
+    <div className="relative w-full items-center justify-center flex flex-col bg-[#1d1d21] md:aspect-[768/643] aspect-[360/400] pt-[16vw] lg:pt-[8vw] lg:px-[11.979vw] lg:aspect-[1920/600]">
+      <div>
+        <Image
+          src="/img/yogyakarta/description/ornamentlg.svg"
+          width={1}
+          height={1}
+          alt="Picture of the author"
+          className="lg:w-[44.105vw] mb-[2vw]"
+          data-aos="fade-up"
+          data-aos-duration={aosDuration}
+          data-aos-delay={getDelay({ turn: 0 })}
+        ></Image>
+      </div>
+      <div className="relative items-center justify-center flex flex-col lg:flex-row mb-[16vw]">
+        <div
+          data-aos="fade-up"
+          data-aos-duration={aosDuration}
+          data-aos-delay={getDelay({ turn: 1 })}
+        >
+          <Image
+            src="/img/yogyakarta/description/flower.svg"
+            width={1}
+            height={1}
+            alt="Picture of the author"
+            className="w-[17.944vw] md:w-[11.328vw] lg:w-[30vw] rotate-90 lg:rotate-0"
+          ></Image>
+        </div>
+        <div
+          className="m-[6.944vw] md:my-[3.125vw] md:mx-[10.547vw]  rounded-[6.667vw] lg:m-0 lg:rounded-[1.354vw] md:rounded-[3.646vw] border-white border-opacity-20 border-[0.278vw] lg:border-[0.052vw] bg-white bg-opacity-[0.01] flex justify-center items-center text-center"
+          data-aos="fade-up"
+          data-aos-duration={aosDuration}
+          data-aos-delay={getDelay({ turn: 2 })}
+        >
+          <p className="font-thunder text-[5vw] m-[6.3vw] md:m-[3.385vw] my-[12vw] lg:mx-[4.427vw] lg:my-[1.979vw] md:text-[2.344vw] lg:text-[1.458vw]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div
+          data-aos="fade-up"
+          data-aos-duration={aosDuration}
+          data-aos-delay={getDelay({ turn: 3 })}
+        >
+          <Image
+            src="/img/yogyakarta/description/flower.svg"
+            width={1}
+            height={1}
+            alt="Picture of the author"
+            className="w-[17.944vw]  md:w-[11.328vw] lg:w-[30vw] -rotate-90 lg:-rotate-180"
+          ></Image>
+        </div>
+      </div>
+      <div
+        className=" font-thunderbold text-[19.817vw] md:text-[9.289vw] lg:text-[17.587vw] my-[16vw] md:my-[5vw] lg:my-[7vw] leading-none tracking-widest bg-gradient-to-b from-white to-[#1D1D21] inline-block text-transparent bg-clip-text text-center"
+        data-aos="fade-up"
+        data-aos-duration={aosDuration}
+        data-aos-delay={getDelay({ turn: 4 })}
+      >
+        SOROTAN
+      </div>
+    </div>
+  );
+}
--- components/pages/yogyakarta/Next_Prev/Next_Prev.js
+++ components/pages/yogyakarta/Next_Prev/Next_Prev.js
@@ -1,7 +1,93 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import Image from "next/image";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { aosDuration, getDelay } from "../../../allPage/configs/aosConfig";
 
 export default function Next_Prev() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+  const [hoveredButton, setHoveredButton] = useState(null);
+
   return (
-    <div className="relative w-full items-center justify-center flex md:aspect-[1252/557]"></div>
+    <div className="relative w-full items-center justify-end flex aspect-[360/320] md:aspect-[1252/557] bg-[#1d1d21] overflow-hidden flex-col z-[9999999999]">
+      <button
+        className="absolute -left-[26vw] md:-left-[15vw] lg:-left-[20vw] -bottom-[18vw] md:-bottom-[18vw] lg:-bottom-[20vw] w-[90vw] md:w-[50vw] lg:w-[60.677vw] transition-transform duration-300 ease-in-out transform hover:-translate-y-[8vw] z-[99]"
+        onMouseEnter={() => setHoveredButton("prev")}
+        onMouseLeave={() => setHoveredButton(null)}
+        aria-label="Previous"
+      >
+        <Image
+          src="/img/yogyakarta/next_prev/prev-sleman.svg"
+          width={1}
+          height={1}
+          alt="Picture of the author"
+          className="w-[90vw] md:w-[50vw] lg:w-[60.677vw] transition-transform duration-300 ease-in-out z-[9999999999]"
+        />
+      </button>
+      <button
+        className="absolute -right-[30vw] md:-right-[15vw] lg:-right-[20vw] -bottom-[18vw] md:-bottom-[18vw] lg:-bottom-[20vw] w-[90vw] md:w-[50vw] lg:w-[60.677vw] z-[1] transition-transform duration-300 ease-in-out transform hover:-translate-y-[8vw]"
+        onMouseEnter={() => setHoveredButton("next")}
+        onMouseLeave={() => setHoveredButton(null)}
+        aria-label="Next"
+      >
+        <Image
+          src="/img/yogyakarta/next_prev/next-bantul.svg"
+          width={1}
+          height={1}
+          alt="Picture of the author"
+          className="w-[90vw] md:w-[50vw] lg:w-[60.677vw] transition-transform duration-300 ease-in-out"
+        />
+      </button>
+      <div
+        className="flex flex-row justify-center items-start mb-[2vw] z-[99999]"
+        data-aos="fade-up"
+        data-aos-duration={aosDuration}
+        data-aos-delay={getDelay({ turn: 0 })}
+      >
+        <div>
+          <Image
+            src="/img/yogyakarta/next_prev/prev.svg"
+            width={1}
+            height={1}
+            alt="Picture of the author"
+            className={`w-[3.2vw] md:w-[1.5vw] lg:w-[1.677vw] transition-transform duration-300 ease-in-out z-[999999] ${
+              hoveredButton === "next" ? "invisible" : ""
+            }`}
+          />
+        </div>
+        <div className=" font-thundermedium text-[5vw] md:text-[2.344vw] lg:text-[2.778vw] md:mr-[0.5vw] md:ml-[0.5vw] mr-[3vw] ml-[3vw] leading-none bg-gradient-to-b from-white to-[#1D1D21] inline-block text-transparent bg-clip-text w-[30vw] md:w-[16vw] lg:w-[18vw] text-center">
+          {hoveredButton === "prev"
+            ? "SLEMAN"
+            : hoveredButton === "next"
+              ? "BANTUL"
+              : "TERUS MENJELAJAH"}
+        </div>
+        <div>
+          <Image
+            src="/img/yogyakarta/next_prev/next.svg"
+            width={1}
+            height={1}
+            alt="Picture of the author"
+            className={`w-[3.2vw] md:w-[1.5vw] lg:w-[1.677vw] transition-transform duration-300 ease-in-out ${
+              hoveredButton === "prev" ? "invisible" : ""
+            }`}
+          />
+        </div>
+      </div>
+      <div>
+        <Image
+          src="/img/yogyakarta/next_prev/ornamentlg.svg"
+          width={1}
+          height={1}
+          alt="Picture of the author"
+          className="w-[50vw] md:w-[20vw] lg:w-[28.677vw] transition-transform duration-300 ease-in-out mb-[4vw]"
+          data-aos="fade-up"
+          data-aos-duration={aosDuration}
+          data-aos-delay={getDelay({ turn: 0 })}
+        />
+      </div>
+    </div>
   );
 }
