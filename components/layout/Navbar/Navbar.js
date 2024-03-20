93335244ca086332c7e27b006d88ebdf726d1e7e
Muhammad Grandiv <grandivfarand@gmail.com>
2024-03-20 21:11:08 +0700

    fix navbar width

--- components/layout/Navbar/Navbar.js
+++ components/layout/Navbar/Navbar.js
@@ -32,7 +32,7 @@ export function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <div className="flex sticky top-0 items-center justify-between py-8 mr-[2vw] ml-[2vw] z-[99999999999999999]">
+    <div className="flex sticky top-0 items-center justify-between py-8 z-[99999999999999999]">
       <Link href="/">
         <Image
           src={
@@ -41,21 +41,23 @@ export function Navbar() {
           alt="logo"
           width={100}
           height={100}
-          className="z-9999999 logo w-[40vw] md:w-[20vw] lg:w-[10vw]"
+          className="ml-[2vw] z-9999999 logo w-[40vw] md:w-[20vw] lg:w-[10vw]"
         />
       </Link>
       <nav>
         <section className="flex inset-0">
           <div
             className="space-y-2"
-            onClick={() => setIsNavOpen((prev) => !prev)}>
-            <FaBars className="z-[999999999999999999] text-[#EDD761] text-[6vw] md:text-[4vw] lg:text-[2vw] hover:cursor-pointer" />
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          >
+            <FaBars className="mr-[2vw] z-[999999999999999999] text-[#EDD761] text-[6vw] md:text-[4vw] lg:text-[2vw] hover:cursor-pointer" />
           </div>
 
           <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
             <div
               className="absolute top-0 right-0 px-8 py-8 hover:cursor-pointer hover:bg-white rounded-full"
-              onClick={() => setIsNavOpen((prev) => !prev)}>
+              onClick={() => setIsNavOpen((prev) => !prev)}
+            >
               <svg
                 className="h-8 w-8 text-gray-600"
                 viewBox="0 0 24 24"
@@ -63,7 +65,8 @@ export function Navbar() {
                 stroke="currentColor"
                 strokeWidth="2"
                 strokeLinecap="round"
-                strokeLinejoin="round">
+                strokeLinejoin="round"
+              >
                 <line x1="18" y1="6" x2="6" y2="18" />
                 <line x1="6" y1="6" x2="18" y2="18" />
               </svg>
@@ -75,7 +78,8 @@ export function Navbar() {
                   setTimeout(() => {
                     setIsNavOpen((prev) => !prev);
                   }, 300);
-                }}>
+                }}
+              >
                 <Link href="/">Beranda</Link>
               </div>
               <div className="border-gray-400 space-y-[2vw] lg:space-y-0 uppercase flex flex-col items-center">
@@ -86,13 +90,15 @@ export function Navbar() {
                   <Link
                     className="font-thunder text-[24px] lg:text-[32px] text-[#EDD761] hover:text-[#F5F5F5]"
                     href={item.link}
-                    key={index}>
+                    key={index}
+                  >
                     <span
                       onClick={() => {
                         setTimeout(() => {
                           setIsNavOpen((prev) => !prev);
                         }, 300);
-                      }}>
+                      }}
+                    >
                       {item.title}
                     </span>
                   </Link>
